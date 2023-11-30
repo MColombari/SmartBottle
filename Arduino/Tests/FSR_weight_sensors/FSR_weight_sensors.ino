57be298510b5f38414c44d4293924d34811a6556
MattiaColombari <mattia.colombari00@gmail.com>
2023-11-30 16:05:11 +0100

    Improved for 4 sensor, and multiplotting in Arduino serial.

--- Arduino/Tests/FSR_weight_sensors/FSR_weight_sensors.ino
+++ Arduino/Tests/FSR_weight_sensors/FSR_weight_sensors.ino
@@ -1,40 +1,61 @@
-#define IN_PIN A1
-#define PRINT_PERIOD 100
+#define IN_PIN_0 A0
+#define IN_PIN_1 A1
+#define IN_PIN_2 A2
+#define IN_PIN_3 A3
+#define FILTER_DIMENSION 100
+#define PRINT_PERIOD 1000
+
+/*
+  Description: for the breadbord test i chose for each resistor a 10K pull-up resistor.
+*/
 
 unsigned long start_time;
 
-float values[100];    // Digital filter.
+float values_0[FILTER_DIMENSION];    // Digital filter 0.
+float values_1[FILTER_DIMENSION];    // Digital filter 1.
+float values_2[FILTER_DIMENSION];    // Digital filter 2.
+float values_3[FILTER_DIMENSION];    // Digital filter 3.
 
 int index;
 
-double means(){
+double means(float in[FILTER_DIMENSION]){
   double ret = 0;
-  for(int i = 0; i < 100; i++){
-    ret += values[i];
+  for(int i = 0; i < FILTER_DIMENSION; i++){
+    ret += in[i];
   }
-  return ret / 100;
+  return ret / FILTER_DIMENSION;
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   start_time = millis();
+
   index = 0;
-  for (int i = 0; i < 100; i++){
-    values[i] = 0;
+
+  for (int i = 0; i < FILTER_DIMENSION; i++){
+    values_0[i] = 0;
+    values_1[i] = 0;
+    values_2[i] = 0;
+    values_3[i] = 0;
   }
 }
 
 void loop() {
-  if(index >= 100){
+  if(index >= FILTER_DIMENSION){
     index = 0;
   }
-  values[index] = analogRead(IN_PIN);
+  values_0[index] = analogRead(IN_PIN_0);
+  values_1[index] = analogRead(IN_PIN_1);
+  values_2[index] = analogRead(IN_PIN_2);
+  values_3[index] = analogRead(IN_PIN_3);
   index++;
   // put your main code here, to run repeatedly:
   if(PRINT_PERIOD <= millis() - start_time){
-    double value = means();
-    Serial.println(value);
+    Serial.print(means(values_0)); Serial.print(",");
+    Serial.print(means(values_1)); Serial.print(",");
+    Serial.print(means(values_2)); Serial.print(",");
+    Serial.println(means(values_3));
     start_time = millis();
   }
 }
